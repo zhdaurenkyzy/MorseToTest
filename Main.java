bf8cc7958c6550cd52a03c434f1d44d8e2641412
zhdaurenkyzy <51210391+zhdaurenkyzy@users.noreply.github.com>
2019-06-15 00:59:17 +0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.HashMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Conversion conversion = new Conversion();
+        conversion.morseToText();
+        //conversion.textToMorse();
+    }
+}
